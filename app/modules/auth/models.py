11915a32293d8d9942c9d869dc4ebad1b820aa80
Maria Escalante <marescram3@alum.us.es>
2024-11-12 22:28:46 +0100

    Feat: modelo de datos y vistas

--- app/modules/auth/models.py
+++ app/modules/auth/models.py
@@ -1,9 +1,12 @@
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
+
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from flask import current_app
 
 
 class User(db.Model, UserMixin):
@@ -15,6 +18,9 @@ class User(db.Model, UserMixin):
 
     data_sets = db.relationship('DataSet', backref='user', lazy=True)
     profile = db.relationship('UserProfile', backref='user', uselist=False)
+    
+    reset_token = db.Column(db.String(256), nullable=True)
+    reset_token_expiration = db.Column(db.DateTime, nullable=True)
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -33,3 +39,20 @@ class User(db.Model, UserMixin):
     def temp_folder(self) -> str:
         from app.modules.auth.services import AuthenticationService
         return AuthenticationService().temp_folder_by_user(self)
+    
+    def generate_reset_token(self, expires_minutes=15):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        token = s.dumps({'user_id': self.id})
+        self.reset_token = token
+        self.reset_token_expiration = datetime.now() + timedelta(minutes=expires_minutes)
+        db.session.commit()
+        return token
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token, max_age=1800)['user_id']  # Aquí se define la expiración en segundos
+        except Exception:
+            return None
+        return User.query.get(user_id)
