ee5eae9ee2fbd1e7a96df4beb20771cc39002a46
Manuel Palacios Pineda <manpalpin@alum.us.es>
2024-11-13 01:02:45 +0100

    feat: acceso al dashboard
    Se ha añadido un método de acceso al dashboard mediante el desplegable de usuario. Se ha reforzado la seguridad del dashboard y se ha añadido una nueva estadística.

--- app/modules/dataset/services.py
+++ app/modules/dataset/services.py
@@ -104,13 +104,13 @@ class DataSetService(BaseService):
 
     def max_downloads(self):
         return self.dsdownloadrecord_repository.max_downloads()
-    
+
     def datasets_with_most_downloads(self):
         return self.dsdownloadrecord_repository.datasets_with_most_downloads()
-    
+
     def users_with_most_downloads(self):
         return self.dsdownloadrecord_repository.users_with_most_downloads()
-    
+
     def datasets_with_most_views(self):
         return self.dsviewrecord_repostory.datasets_with_most_views()
 
