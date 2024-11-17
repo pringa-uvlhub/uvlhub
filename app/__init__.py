93b8df2520ddbfb5f550428007d6e04892b2b7ab
Maria Escalante <marescram3@alum.us.es>
2024-11-17 20:18:12 +0100

    Fix: codacy check v3

--- app/__init__.py
+++ app/__init__.py
@@ -30,6 +30,7 @@ def create_app(config_name='development'):
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'pringauvlhub@gmail.com'
     app.config['MAIL_PASSWORD'] = 'ffiq pmcq bvko tndg'
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     app.config['MAIL_DEFAULT_SENDER'] = 'pringauvlhub@gmail.com'
     mail.init_app(app)  # Inicializamos el mail con la aplicación
 
