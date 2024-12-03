91ec0f7785e776828e47e04df27e048218152793
Francisco Mateos Villarejo <framatvil@alum.us.es>
2024-12-03 15:49:21 +0100

    Feat: users and datasets with more downloads finished

--- app/modules/admin/routes.py
+++ app/modules/admin/routes.py
@@ -3,7 +3,10 @@ from flask_login import login_required, current_user
 from app.modules.admin import admin_bp
 from app.modules.featuremodel.services import FeatureModelService
 from app.modules.dataset.services import DataSetService
+from app.modules.hubfile.services import HubfileService, HubfileDownloadRecordService
 from app.modules.auth.services import AuthenticationService
+from app.modules.charts.charts import plot_downloads_bar_chart, plot_users_with_most_downloads, plot_feature_models_with_most_downloads
+
 
 
 @admin_bp.route('/admin', methods=['GET'])
@@ -20,6 +23,7 @@ def dashboard():
 
     dataset_service = DataSetService()
     feature_model_service = FeatureModelService()
+    hubfile_download_service= HubfileDownloadRecordService()
     auth_service = AuthenticationService()
 
     # Statistics: total datasets and feature models
@@ -29,7 +33,9 @@ def dashboard():
     # Statistics: total downloads
     total_dataset_downloads = dataset_service.total_dataset_downloads()
     total_feature_model_downloads = feature_model_service.total_feature_model_downloads()
+    fm_downloads = hubfile_download_service.features_models_with_most_downloads()
     max_downloads = dataset_service.max_downloads()
+    #datasets_with_most_downloads = dataset_service.datasets_with_most_downloads()
     user_downloads = dataset_service.user_max_downloads()
 
     # Statistics: total views
@@ -40,6 +46,18 @@ def dashboard():
     users_counter = auth_service.count_total_users()
     admin_user_counter = auth_service.count_total_admin_users()
 
+    # Plot datasets with most dowloads
+    dataset_names, download_counts = dataset_service.datasets_with_most_downloads()
+    plot_url = plot_downloads_bar_chart(dataset_names, download_counts)
+
+    #Plot users with most downloads
+    users_email, downloads = dataset_service.users_with_most_downloads()
+    plot_users = plot_users_with_most_downloads(users_email, downloads)
+
+    #Plot feature models with most downloads
+    features_title, download_feature = hubfile_download_service.features_models_with_most_downloads()
+    plot_features_downloads = plot_feature_models_with_most_downloads(features_title, download_feature)
+
     return render_template(
         "admin/dashboard.html",
         datasets=dataset_service.latest_synchronized(),
@@ -49,10 +67,15 @@ def dashboard():
         total_feature_model_downloads=total_feature_model_downloads,
         total_dataset_views=total_dataset_views,
         total_feature_model_views=total_feature_model_views,
+        fm_downloads = fm_downloads,
         users_counter=users_counter,
         admin_user_counter=admin_user_counter,
         max_downloads_dataset=max_downloads[0],
         max_downloads=max_downloads[1],
+        #datasets_with_most_downloads = datasets_with_most_downloads,
         user_downloads=user_downloads[0],
-        number_downloads_user=user_downloads[1]
+        number_downloads_user=user_downloads[1],
+        plot_url=plot_url,
+        plot_users = plot_users,
+        plot_features_downloads = plot_features_downloads
     )
--- app/modules/dataset/services.py
+++ app/modules/dataset/services.py
@@ -91,6 +91,12 @@ class DataSetService(BaseService):
 
     def max_downloads(self):
         return self.dsdownloadrecord_repository.max_downloads()
+    
+    def datasets_with_most_downloads(self):
+        return self.dsdownloadrecord_repository.datasets_with_most_downloads()
+    
+    def users_with_most_downloads(self):
+        return self.dsdownloadrecord_repository.users_with_most_downloads()
 
     def user_max_downloads(self):
         return self.dsdownloadrecord_repository.user_max_downloads()
