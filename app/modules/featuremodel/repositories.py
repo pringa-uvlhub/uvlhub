e1ab0ed9695cc57cff8a197c3dca0f2c720c4b86
Francisco Mateos Villarejo <framatvil@alum.us.es>
2024-12-03 15:51:13 +0100

    Feat: top 5 feature models with mot downloads

--- app/modules/featuremodel/repositories.py
+++ app/modules/featuremodel/repositories.py
@@ -11,6 +11,11 @@ class FeatureModelRepository(BaseRepository):
     def count_feature_models(self) -> int:
         max_id = self.model.query.with_entities(func.max(self.model.id)).scalar()
         return max_id if max_id is not None else 0
+    
+    def get_feature_model_by_id(self, feature_model_id: int):
+        return self.model.query.filter_by(id=feature_model_id).first()
+    
+    
 
 
 class FMMetaDataRepository(BaseRepository):
--- app/modules/featuremodel/services.py
+++ app/modules/featuremodel/services.py
@@ -7,6 +7,7 @@ class FeatureModelService(BaseService):
     def __init__(self):
         super().__init__(FeatureModelRepository())
         self.hubfile_service = HubfileService()
+        self.repository = FeatureModelRepository()
 
     def total_feature_model_views(self) -> int:
         return self.hubfile_service.total_hubfile_views()
@@ -16,6 +17,12 @@ class FeatureModelService(BaseService):
 
     def count_feature_models(self):
         return self.repository.count_feature_models()
+    
+    def get_feature_model_by_id(self, feature_model_id: int):
+        return self.repository.get_feature_model_by_id(feature_model_id)
+    
+    def feature_models_with_most_downloads(self):
+        return self.repository.feature_models_with_most_downloads()
 
     class FMMetaDataService(BaseService):
         def __init__(self):
