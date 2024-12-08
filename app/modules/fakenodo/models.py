ba15138120ff8c4b6007d52c86880e237dc850a4
Javier Nieto <javiernieto03.jnv@gmail.com>
2024-12-08 22:15:41 +0100

    Feat: Created fakenodo API first version

--- app/modules/fakenodo/models.py
+++ app/modules/fakenodo/models.py
@@ -3,7 +3,6 @@ from app import db
 
 class Fakenodo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(500), nullable=False)
-    creators = db.Column(db.String(255), nullable=False)
-    doi = db.Column(db.String(100), nullable=True)
+
+    def __repr__(self):
+        return f'Fakenodo<{self.id}>'
--- app/modules/fakenodo/services.py
+++ app/modules/fakenodo/services.py
@@ -1,15 +1,7 @@
-import os
 from app.modules.fakenodo.repositories import FakenodoRepository
 from core.services.BaseService import BaseService
 
 
 class FakenodoService(BaseService):
-    def get_fakenodo_access_token(self):
-        return os.getenv("ZENODO_ACCESS_TOKEN")
-    
     def __init__(self):
         super().__init__(FakenodoRepository())
-        self.FAKENODO_ACCESS_TOKEN = self.get_fakenodo_access_token()
-        # self.FAKENODO_API_URL = self.get_zenodo_url()
-        self.headers = {"Content-Type": "application/json"}
-        self.params = {"access_token": self.FAKENODO_ACCESS_TOKEN}
