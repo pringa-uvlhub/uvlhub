4d4683a2f6f29524507e1145354d5c6c44324f14
Javier Nieto <javiernieto03.jnv@gmail.com>
2024-12-09 03:08:16 +0100

    Fix: Configuring env variables and github actions

--- app/__init__.py
+++ app/__init__.py
@@ -29,7 +29,7 @@ def create_app(config_name='development'):
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'pringauvlhub@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'ffiq pmcq bvko tndg'
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     app.config['MAIL_DEFAULT_SENDER'] = 'pringauvlhub@gmail.com'
     mail.init_app(app)  # Inicializamos el mail con la aplicación
 
@@ -40,7 +40,7 @@ def create_app(config_name='development'):
     # Inicializar SQLAlchemy y Migrate con la app
     db.init_app(app)
     migrate.init_app(app, db)
-    app.config['SECRET_KEY'] = 'secret_key'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
     # Registrar módulos
     module_manager = ModuleManager(app)
