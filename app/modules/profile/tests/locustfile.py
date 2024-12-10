a2f201e0a16dc35d7bd89919346927ff0cb100ce
Javier Garcia <javgarciar@gmail.com>
2024-12-10 14:06:56 +0100

    Test: added locustfile file for testing view user profile

---
+++ app/modules/profile/tests/locustfile.py
@@ -0,0 +1,15 @@
+from locust import HttpUser, TaskSet, task, between
+
+class UserProfileTasks(TaskSet):
+    @task
+    def view_user_profile(self):
+        user_id = 1  
+        with self.client.get(f"/profile/{user_id}", name="/profile/[user_id]", catch_response=True) as response:
+            if response.status_code == 200 and "user_profile" in response.text:
+                response.success()
+            else:
+                response.failure(f"Failed to load user profile for user_id={user_id}")
+
+class WebsiteUser(HttpUser):
+    tasks = [UserProfileTasks]
+    wait_time = between(1, 5)  
