777eedd21c91862b63622cd03de2928437ffb701
Miguel González Ortiz <miggonort1@alum.us.es>
2024-12-11 20:15:33 +0100

    Test: Selenium test for rating dataset added

--- app/modules/dataset/tests/test_selenium_ratedataset.py
+++ app/modules/dataset/tests/test_selenium_ratedataset.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 
 class TestRatedataset:
@@ -13,14 +14,15 @@ class TestRatedataset:
 
     def test_ratedataset(self):
         self.driver.get("http://localhost:5000/")
-        self.driver.set_window_size(1854, 1048)
-        self.driver.find_element(By.LINK_TEXT, "Login").click()
+        self.driver.set_window_size(1702, 963)
+        self.driver.find_element(By.CSS_SELECTOR, ".nav-link:nth-child(1)").click()
+        self.driver.find_element(By.ID, "email").click()
         self.driver.find_element(By.ID, "email").send_keys("user1@example.com")
+        self.driver.find_element(By.ID, "password").click()
         self.driver.find_element(By.ID, "password").send_keys("1234")
         self.driver.find_element(By.ID, "submit").click()
         self.driver.find_element(By.LINK_TEXT, "Sample dataset 4").click()
-        self.driver.find_element(By.CSS_SELECTOR, "#star-rating-12 > span:nth-child(2)").click()
+        time.sleep(1)
+        self.driver.find_element(By.CSS_SELECTOR, "#star-rating-4 > span:nth-child(4)").click()
+        time.sleep(1)
         self.driver.find_element(By.CSS_SELECTOR, "div:nth-child(1) > button").click()
-
-        # Verificar éxito
-        assert "Rating added" in self.driver.page_source, "El mensaje de calificación no apareció."
