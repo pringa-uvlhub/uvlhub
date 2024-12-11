773a06b60e4d501762335b6097ec9b525709b976
Javier Garcia <javgarciar@gmail.com>
2024-12-11 16:11:08 +0100

    Fix: more lint fixes

--- app/modules/profile/tests/locustfile.py
+++ app/modules/profile/tests/locustfile.py
@@ -4,7 +4,7 @@ from locust import HttpUser, TaskSet, task, between
 class UserProfileTasks(TaskSet):
     @task
     def view_user_profile(self):
-        user_id = 1  
+        user_id = 1
         with self.client.get(f"/profile/{user_id}", name="/profile/[user_id]", catch_response=True) as response:
             if response.status_code == 200 and "user_profile" in response.text:
                 response.success()
