0b3c8ec05780a8c94833e2990941e3ac048bc5c3
Luis Periañez <luiperfra1@alum.us.es>
2024-12-13 18:31:38 +0100

    Fix: Improve .py Community

--- app/modules/community/forms.py
+++ app/modules/community/forms.py
@@ -6,13 +6,9 @@ from wtforms.validators import DataRequired
 class CommunityForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     description = TextAreaField("Description", validators=[DataRequired()])
-
-    # Logo field: Optional, but if provided, it must be an .svg file
-
     submit = SubmitField('Save community')
 
     def get_dsmetadata(self):
-
         return {
             "name": self.name.data,
             "description": self.description.data,
--- app/modules/community/seeders.py
+++ app/modules/community/seeders.py
@@ -6,71 +6,66 @@ from datetime import datetime, timezone
 
 class CommunitySeeder(BaseSeeder):
 
-    priority = 2  # Prioridad menor que AuthSeeder si los usuarios ya deben existir.
+    priority = 2
 
     def run(self):
 
-        # Busca usuarios existentes para asociarlos con comunidades.
         users = User.query.all()
 
         if not users:
             print("No users found. Make sure AuthSeeder has run before this.")
             return
 
-        # Mapear usuarios a IDs
         user_ids = [user.id for user in users]
 
-        # Si no hay suficientes usuarios, usa un ID ficticio o maneja la excepción.
         if len(user_ids) < 2:
             print("Not enough users to assign to communities. Check AuthSeeder.")
             return
 
-        # Datos de ejemplo para comunidades.
         communities = [
             Community(
                 name="Open Source Enthusiasts",
                 description="A community for developers passionate about open source projects.",
-                created_by_id=user_ids[0],  # Asigna al primer usuario como creador.
+                created_by_id=user_ids[0],
                 created_at=datetime.now(timezone.utc),
                 logo=None,
                 admin_by_id=user_ids[0],
-                users=[users[0]]  # Añade al creador como miembro automáticamente
+                users=[users[0]]
             ),
             Community(
                 name="AI Researchers",
                 description="A group for discussing advancements in AI and machine learning.",
-                created_by_id=user_ids[1],  # Usa otro usuario si está disponible.
+                created_by_id=user_ids[1],
                 created_at=datetime.now(timezone.utc),
                 logo=None,
                 admin_by_id=user_ids[1],
-                users=[users[1]]  # Añade al creador como miembro automáticamente
+                users=[users[1]]
             ),
-
             Community(
                 name="Scientific Community",
                 description=(
-                        "Explore scientific breakthroughs, collaborate on research papers, and connect "
-                        "with leading academics in various fields of science and technology."
+                    "Explore scientific breakthroughs, collaborate on research papers, and connect "
+                    "with leading academics in various fields of science and technology."
                 ),
-                created_by_id=user_ids[0],  # Usar el primer usuario.
+                created_by_id=user_ids[0],
                 created_at=datetime.now(timezone.utc),
-                logo=None,  # No se asigna logo, utilizará el predeterminado.
+                logo=None,
                 admin_by_id=user_ids[0],
-                users=[users[0]]  # Añade al creador como miembro automáticamente
+                users=[users[0]]
             ),
             Community(
                 name="Dark Matter",
                 description="A group for discussing advancements in Dark Matter.",
-                created_by_id=user_ids[1],  # Usa otro usuario si está disponible.
+                created_by_id=user_ids[1],
                 created_at=datetime.now(timezone.utc),
                 logo=None,
                 admin_by_id=user_ids[1],
-                users=[users[1]]  # Añade al creador como miembro automáticamente
+                users=[users[1]]
             ),
             Community(
                 name="Biology",
                 description="A group for discussing advancements in biology.",
-                created_by_id=user_ids[1],  # Usa otro usuario si está disponible.
+                created_by_id=user_ids[1],
                 created_at=datetime.now(timezone.utc),
                 logo=None,
                 admin_by_id=user_ids[1],
@@ -79,7 +74,7 @@ class CommunitySeeder(BaseSeeder):
             Community(
                 name="Atomic",
                 description="A group for discussing advancements in atomic.",
-                created_by_id=user_ids[0],  # Usa otro usuario si está disponible.
+                created_by_id=user_ids[0],
                 created_at=datetime.now(timezone.utc),
                 logo=None,
                 admin_by_id=user_ids[0],
@@ -87,7 +82,4 @@ class CommunitySeeder(BaseSeeder):
             ),
         ]
 
-        # Insertar comunidades en la base de datos.
         self.seed(communities)
-
-        # Después de crear las comunidades, agregar los creadores como miembros.
